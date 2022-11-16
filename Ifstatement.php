7e826ae8391cc537d1a87a2c8fafb2d9db2a3c71
Damario Roberts <61894191+BeckhamDR@users.noreply.github.com>
2022-11-15 19:57:09 -0500

    Update Ifstatement.php

--- Ifstatement.php
+++ Ifstatement.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include 'include/header.php' ?>
+<?php include 'include/header.php'?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -39,5 +39,5 @@ $age = '';
 
 
 ?>
-<?php require 'include/footer.php' ?>
+<?php require 'include/footer.php'?>
 </html>
